bd3d3c8ae136644caf5d043ac3fc758777582fb1
Melissa Chang <melissachang@users.noreply.github.com>
2018-03-14 15:20:32 -0700

    Configure Elasticsearch URL

--- api/data_explorer/controllers/facets_controller.py
+++ api/data_explorer/controllers/facets_controller.py
@@ -1,6 +1,9 @@
 from datetime import date
 import json
 
+from elasticsearch import Elasticsearch
+from flask import current_app
+
 from data_explorer.models.facet import Facet
 from data_explorer.models.facet_value import FacetValue
 from ..dataset_faceted_search import DatasetFacetedSearch
@@ -11,9 +14,12 @@ def facets_get():
     Returns facets.
 
     Returns:
-        List of Facet.
+        List of Facets.
     """
     search = DatasetFacetedSearch()
+    search.using = Elasticsearch(current_app.config['ELASTICSEARCH_URL'])
+    # Need to rebuild search._s with new s.using.
+    search.__init__()
     response = search.execute()
     facets = []
     for facet_name, values in response.facets.to_dict().iteritems():
--- api/data_explorer/dataset_faceted_search.py
+++ api/data_explorer/dataset_faceted_search.py
@@ -2,7 +2,6 @@
 
 import json
 
-from elasticsearch import Elasticsearch
 from elasticsearch_dsl import FacetedSearch
 from elasticsearch_dsl import TermsFacet
 
@@ -18,9 +17,20 @@ def _get_index_name():
 
 
 class DatasetFacetedSearch(FacetedSearch):
+    """Subclass of FacetedSearch for Datasets.
+
+    This class body is run when module is loaded; there is no app context yet.
+    App context contains configuration such as Elasticsearch URL. To use this
+    class, specify Elasticsearch URL like so:
+
+        search = DatasetFacetedSearch()
+        search.using = Elasticsearch(current_app.config['ELASTICSEARCH_URL'])
+        # Need to rebuild search._s with new s.using.
+        search.__init__()
+        response = search.execute()
+    """
     index = _get_index_name()
-    using = Elasticsearch('elasticsearch:9200')
-    # TODO(melissachang): Figure out facets from index.
+    # TODO(melissachang): Get facets from facet_fields.csv.
     facets = {
         'Gender': TermsFacet(field='Gender.keyword'),
         'Region': TermsFacet(field='Region.keyword'),
