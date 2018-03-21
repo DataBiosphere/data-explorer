dd4d3c3bfd2ad499f9abe332b3b701fc0b7ef2c9
Melissa Chang <melissachang@users.noreply.github.com>
2018-03-20 17:16:05 -0700

    Read facets from facet_fields.csv

--- api/data_explorer/dataset_faceted_search.py
+++ api/data_explorer/dataset_faceted_search.py
@@ -1,5 +1,6 @@
 """Subclass of FacetedSearch for Data explorer datasets."""
 
+import csv
 import jsmin
 import json
 
@@ -8,12 +9,13 @@ from flask import current_app
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import HistogramFacet
 from elasticsearch_dsl import FacetedSearch
+from elasticsearch_dsl import Mapping
 from elasticsearch_dsl import TermsFacet
 
 
 def get_index_name():
-    """Gets index name from /app/dataset.json."""
-    with open('/app/dataset.json') as f:
+    """Gets index name from dataset.json."""
+    with open('/app/config/dataset.json') as f:
         # Remove comments using jsmin, as recommended by JSON creator
         # (https://plus.google.com/+DouglasCrockfordEsq/posts/RK8qyGVaGSr).
         dataset = json.loads(jsmin.jsmin(f.read()))
@@ -21,15 +23,35 @@ def get_index_name():
 
 
 def get_facets():
-    """Gets facets from /app/facet_fields.csv."""
-    return {
-        'Age': HistogramFacet(field='Age', interval=7),
-        # Use ".keyword" because we want aggregation on keyword field, not term
-        # field. See
-        # https://www.elastic.co/guide/en/elasticsearch/reference/6.2/fielddata.html#before-enabling-fielddata
-        'Gender': TermsFacet(field='Gender.keyword'),
-        'Region': TermsFacet(field='Region.keyword'),
-    }
+    """Gets facets from facet_fields.csv.
+
+    app.config['ELASTICSEARCH_URL'], app.config['INDEX_NAME'] must be set before
+    this is called.
+    """
+    f = open('/app/config/facet_fields.csv')
+    # Remove comments using jsmin.
+    csv_str = jsmin.jsmin(f.read())
+    facet_rows = csv.DictReader(iter(csv_str.splitlines()), skipinitialspace=True)
+
+    using = Elasticsearch(current_app.config['ELASTICSEARCH_URL'])
+    mapping = Mapping.from_es(current_app.config['INDEX_NAME'], 'type',
+            using=using).to_dict()
+    current_app.logger.info('dataset_faceted_search mapping %s' % mapping)
+    facets = {}
+    for facet_row in facet_rows:
+        field_name = facet_row['readable_field_name']
+        field_type = mapping['type']['properties'][field_name]['type']
+        if field_type == 'text':
+            # Use ".keyword" because we want aggregation on keyword field, not
+            # term field. See
+            # https://www.elastic.co/guide/en/elasticsearch/reference/6.2/fielddata.html#before-enabling-fielddata
+            facets[field_name] = TermsFacet(field=field_name + '.keyword')
+        else:
+            # Assume numeric type.
+            # TODO: Handle other types.
+            facets[field_name] = HistogramFacet(field=field_name, interval=7)
+    current_app.logger.info('dataset_faceted_search facets: %s' % facets)
+    return facets
 
 
 class DatasetFacetedSearch(FacetedSearch):
