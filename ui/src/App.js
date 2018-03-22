f5fedfc8bf8924d6a91d1b78a79b7c4120ef9637
alahwa <alanhwang@verily.com>
2018-03-22 14:56:30 -0700

    Restructure app

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import { ApiClient, FacetsApi } from 'data_explorer_service';
-import Facet from "../api/src/model/Facet";
-import FacetValue from "../api/src/model/FacetValue";
-import FacetsResponse from "../api/src/model/FacetsResponse";
-import FacetsGrid from "../components/FacetsGrid";
-import Header from "../components/Header";
+import Facet from "./api/src/model/Facet";
+import FacetValue from "./api/src/model/FacetValue";
+import FacetsResponse from "./api/src/model/FacetsResponse";
+import FacetsGrid from "./components/FacetsGrid";
+import Header from "./components/Header";
 
 import React, { Component } from 'react';
 import { MuiThemeProvider } from "material-ui";
