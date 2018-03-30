cb6f05c41b024ad9b6cda1eb4fca327bd1dac959
Melissa Chang <melissachang@users.noreply.github.com>
2018-03-29 17:39:25 -0700

    Export to Saturn static button
    FAB tooltips aren't implemented until Material v1
    (https://github.com/mui-org/material-ui/issues/2230#issuecomment-331586991)
    so let's leave out tooltip for now.

---
+++ ui/src/components/ExportFab.js
@@ -0,0 +1,18 @@
+/** Export to Saturn FAB */
+
+import './ExportFab.css';
+
+import FileCloudUpload from 'material-ui/svg-icons/file/cloud-upload';
+import { FloatingActionButton } from "material-ui";
+import React from 'react';
+import {white} from 'material-ui/styles/colors';
+
+function ExportFab(props) {
+    return (
+        <FloatingActionButton className="exportFab">
+            <FileCloudUpload color={white} />
+        </FloatingActionButton>
+    );
+}
+
+export default ExportFab;
