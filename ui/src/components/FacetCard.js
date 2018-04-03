b8754267f86198c9d745a74a5a2a3f51fe60aa0e
Alan Hwang <alanhwang@google.com>
2018-04-03 15:58:00 -0700

    Set up a linter/formatter for UI code (#52)
    * Add a js linter/formatter precommit hook
    
    * Run the formatter over the ui repo
    
    * Add prettier badge
    
    * Formatting
    
    * Update readme
    
    * Fix

--- ui/src/components/FacetCard.js
+++ ui/src/components/FacetCard.js
@@ -1,68 +1,83 @@
-import './FacetCard.css';
+import "./FacetCard.css";
 
-import React, {Component} from 'react';
-import {Card} from "material-ui/Card";
-import {List, ListItem} from "material-ui/List";
-import {Checkbox} from "material-ui";
+import React, { Component } from "react";
+import { Card } from "material-ui/Card";
+import { List, ListItem } from "material-ui/List";
+import { Checkbox } from "material-ui";
 
 class FacetCard extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.facetValues = this.props.facet.values;
+    this.totalCount = this.props.totalCount;
 
-        this.facetValues = this.props.facet.values;
-        this.totalCount = this.props.totalCount;
+    this.state = {
+      selectedValues: []
+    };
 
-        this.state = {
-            selectedValues: []
-        };
+    this.onValueCheck = this.onValueCheck.bind(this);
+    this.isUnselected = this.isUnselected.bind(this);
+  }
 
-        this.onValueCheck = this.onValueCheck.bind(this);
-        this.isUnselected = this.isUnselected.bind(this);
-    }
-
-    render() {
-        const facetValues = this.props.facet.values.map((facetValue) =>
-            <ListItem
-                className="listItem"
-                key={facetValue.name}
-                leftCheckbox={<Checkbox
-                    onCheck={(event, isInputChecked) => this.onValueCheck(facetValue, isInputChecked)}
-                />}
-                primaryText={<div className={this.isUnselected(facetValue) ? " grayText" : ""}>
-                    <div className="facetValueName">{facetValue.name}</div>
-                    <div className="facetValueCount">{facetValue.count}</div>
-                </div>}/>
-        );
-        return (
-            <Card className="facetCard">
-                <div className="cardHeader">
-                    <div>{this.props.facet.name}</div>
-                    <div className="subHeader">
-                        <span>{this.props.totalCount}</span>
-                        <span className="numberSelected">{this.state.selectedValues.length} / {facetValues.length}</span>
-                    </div>
-                </div>
-                <List>{facetValues}</List>
-            </Card>
-        );
-    }
-
-    /** Update this card's state and trigger the callback on the parent component. */
-    onValueCheck(facetValue, isInputChecked) {
-        let newValues = this.state.selectedValues;
-        if (isInputChecked) {
-            newValues.push(facetValue.name);
-        } else {
-            newValues.splice(newValues.indexOf(facetValue.name), 1);
+  render() {
+    const facetValues = this.props.facet.values.map(facetValue => (
+      <ListItem
+        className="listItem"
+        key={facetValue.name}
+        leftCheckbox={
+          <Checkbox
+            onCheck={(event, isInputChecked) =>
+              this.onValueCheck(facetValue, isInputChecked)
+            }
+          />
         }
-        this.setState({selectedValues: newValues});
-        this.props.updateFacets(this.props.facet.name, facetValue.name, isInputChecked);
-    }
+        primaryText={
+          <div className={this.isUnselected(facetValue) ? " grayText" : ""}>
+            <div className="facetValueName">{facetValue.name}</div>
+            <div className="facetValueCount">{facetValue.count}</div>
+          </div>
+        }
+      />
+    ));
+    return (
+      <Card className="facetCard">
+        <div className="cardHeader">
+          <div>{this.props.facet.name}</div>
+          <div className="subHeader">
+            <span>{this.props.totalCount}</span>
+            <span className="numberSelected">
+              {this.state.selectedValues.length} / {facetValues.length}
+            </span>
+          </div>
+        </div>
+        <List>{facetValues}</List>
+      </Card>
+    );
+  }
 
-    isUnselected(facetValue) {
-        return this.state.selectedValues.length > 0 && this.state.selectedValues.indexOf(facetValue.name) < 0;
+  /** Update this card's state and trigger the callback on the parent component. */
+  onValueCheck(facetValue, isInputChecked) {
+    let newValues = this.state.selectedValues;
+    if (isInputChecked) {
+      newValues.push(facetValue.name);
+    } else {
+      newValues.splice(newValues.indexOf(facetValue.name), 1);
     }
+    this.setState({ selectedValues: newValues });
+    this.props.updateFacets(
+      this.props.facet.name,
+      facetValue.name,
+      isInputChecked
+    );
+  }
+
+  isUnselected(facetValue) {
+    return (
+      this.state.selectedValues.length > 0 &&
+      this.state.selectedValues.indexOf(facetValue.name) < 0
+    );
+  }
 }
 
 export default FacetCard;
--- ui/src/components/FacetsGrid.js
+++ ui/src/components/FacetsGrid.js
@@ -1,33 +1,28 @@
-import './FacetsGrid.css';
+import "./FacetsGrid.css";
 import FacetCard from "./FacetCard";
 
-import React, { Component } from 'react';
-import {GridList, GridTile} from "material-ui/GridList";
+import React, { Component } from "react";
+import { GridList, GridTile } from "material-ui/GridList";
 
 function FacetsGrid(props) {
-    const facets = props.facets;
-    const totalCount = props.totalCount;
-    const updateFacets = props.updateFacets;
-    const facetsList = facets.map((facet) =>
-        <GridTile key={facet.name}>
-            <FacetCard
-                facet={facet}
-                totalCount={totalCount}
-                updateFacets={updateFacets}
-                className='facetCard'
-            />
-        </GridTile>
-    );
-    return (
-        <GridList
-            className="gridList"
-            cols={3}
-            cellHeight="auto"
-            padding={1}
-        >
-            {facetsList}
-        </GridList>
-    );
+  const facets = props.facets;
+  const totalCount = props.totalCount;
+  const updateFacets = props.updateFacets;
+  const facetsList = facets.map(facet => (
+    <GridTile key={facet.name}>
+      <FacetCard
+        facet={facet}
+        totalCount={totalCount}
+        updateFacets={updateFacets}
+        className="facetCard"
+      />
+    </GridTile>
+  ));
+  return (
+    <GridList className="gridList" cols={3} cellHeight="auto" padding={1}>
+      {facetsList}
+    </GridList>
+  );
 }
 
 export default FacetsGrid;
