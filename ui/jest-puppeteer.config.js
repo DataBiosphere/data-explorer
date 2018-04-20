055cf5cc473460934b53dc2549b57f60749eb086
Melissa Chang <melissachang@users.noreply.github.com>
2018-04-20 13:58:44 -0700

    Add jest-puppeteer.config.js for debugging

---
+++ ui/jest-puppeteer.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  launch: {
+//    Uncomment to see console.log executed in browser execution context.
+//    dumpio: true,
+//    Uncomment to see browser.
+//    headless: false,
+//    Uncomment to slow down execution. Unit is ms. Note that until 
+//    https://github.com/smooth-code/jest-puppeteer/issues/36 is fixed, tests
+//    will fail.
+//    slowMo: 1000,
+  },
+}
