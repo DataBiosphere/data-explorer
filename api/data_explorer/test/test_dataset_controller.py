77bac1faac72b571095e0c0ed4363ed3f7f9b8ba
Melissa Chang <melissachang@users.noreply.github.com>
2018-05-02 11:11:01 -0700

    Create BaseTestCase

--- api/data_explorer/test/test_dataset_controller.py
+++ api/data_explorer/test/test_dataset_controller.py
@@ -8,23 +8,18 @@ from flask import json
 from flask_testing import TestCase
 from six import BytesIO
 
-from data_explorer.encoder import JSONEncoder
-from data_explorer.models.dataset_response import DatasetResponse  # noqa: E501
-from data_explorer.test import BaseTestCase
+from data_explorer.test.base_test_case import BaseTestCase
 
 
-class TestDatasetController(TestCase):
+class TestDatasetController(BaseTestCase):
     """DatasetController integration test stubs"""
 
     def create_app(self):
-        logging.getLogger('connexion.operation').setLevel('ERROR')
-        app = connexion.App(__name__, specification_dir='../swagger/')
-        app.app.json_encoder = JSONEncoder
-        app.add_api('swagger.yaml')
-        app.app.config.update({
+        app = super(TestDatasetController, self).create_app()
+        app.config.update({
             'DATASET_CONFIG_DIR': '../config',
         })
-        return app.app
+        return app
 
     def test_dataset_get(self):
         """Test case for dataset_get"""
