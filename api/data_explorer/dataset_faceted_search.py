6e9ca22c51481b50fdc96c6da5dee16f65d102f7
Alan Hwang <alanhwang@verily.com>
2018-06-14 12:15:09 -0700

    Get /api/datset table names from facet_fields.csv

--- api/data_explorer/dataset_faceted_search.py
+++ api/data_explorer/dataset_faceted_search.py
@@ -49,6 +49,17 @@ def get_dataset_name():
         dataset = json.loads(jsmin.jsmin(f.read()))
         return dataset['name']
 
+def get_table_names():
+    """Gets a list of table names from facet_fields.csv.
+
+    current_app.config['DATASET_CONFIG_DIR'] must be set before this is called.
+    """
+    table_names = set()
+    for row in get_facet_rows():
+        table_names.add(row['project_id'] + '.' + row['dataset_id'] + '.' + row['table_name'])
+    tables_list = list(table_names)
+    tables_list.sort()
+    return tables_list
 
 def get_facets():
     """Gets facets from facet_fields.csv.
@@ -56,20 +67,13 @@ def get_facets():
     current_app.config['ELASTICSEARCH_URL'], current_app.config['INDEX_NAME']
     current_app.config['DATASET_CONFIG_DIR'] must be set before this is called.
     """
-    f = open(
-        os.path.join(current_app.config['DATASET_CONFIG_DIR'],
-                     'facet_fields.csv'))
-    # Remove comments using jsmin.
-    csv_str = jsmin.jsmin(f.read())
-    facet_rows = csv.DictReader(
-        iter(csv_str.splitlines()), skipinitialspace=True)
 
     using = Elasticsearch(current_app.config['ELASTICSEARCH_URL'])
     mapping = Mapping.from_es(
         current_app.config['INDEX_NAME'], 'type', using=using).to_dict()
     # Preserve order, so facets are returned in the same order as facet_fields.csv
     facets = OrderedDict()
-    for facet_row in facet_rows:
+    for facet_row in get_facet_rows():
         field_name = facet_row['readable_field_name']
         field_type = mapping['type']['properties'][field_name]['type']
         if field_type == 'text':
@@ -89,6 +93,17 @@ def get_facets():
     current_app.logger.info('dataset_faceted_search facets: %s' % facets)
     return facets
 
+def get_facet_rows():
+    """Parses facet_fields.csv as a list.
+
+    current_app.config['DATASET_CONFIG_DIR'] must be set before this is called.
+    """
+    f = open(
+        os.path.join(current_app.config['DATASET_CONFIG_DIR'],
+                     'facet_fields.csv'))
+    # Remove comments using jsmin.
+    csv_str = jsmin.jsmin(f.read())
+    return csv.DictReader(iter(csv_str.splitlines()), skipinitialspace=True)
 
 class DatasetFacetedSearch(FacetedSearch):
     """Subclass of FacetedSearch for Datasets.
--- api/data_explorer/test/test_dataset_controller.py
+++ api/data_explorer/test/test_dataset_controller.py
@@ -17,7 +17,12 @@ class TestDatasetController(BaseTestCase):
         """Test case for dataset_get"""
         response = self.client.get('/dataset')
         self.assert200(response)
-        self.assertEquals(dict(name='Test data'), response.json)
+        self.assertEquals(dict(name='Test data', tableNames= [
+            "test_project1.test_dataset1.test_table1",
+            "test_project1.test_dataset1.test_table2",
+            "test_project2.test_dataset2.test_table3",
+            "test_project2.test_dataset3.test_table4"
+        ]), response.json)
 
 
 if __name__ == '__main__':
