3286993e74f423819d892552e6e1d3278a3c18f9
melissachang <10929390+melissachang@users.noreply.github.com>
2018-06-20 12:25:15 -0700

    Turn off service worker caching since it doesn't work with IAP (#88)

--- ui/src/index.js
+++ ui/src/index.js
@@ -5,4 +5,7 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+
+// Turn off service worker caching because it doesn't work with Identity-Aware
+// Proxy. See https://github.com/DataBiosphere/data-explorer/pull/88
+// registerServiceWorker();
