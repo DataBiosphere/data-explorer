13c0a753f825dcf296c22898dbfb51f9bd5ec56e
Alan Hwang <alanhwang@verily.com>
2018-07-17 10:14:22 -0700

    Add ExportFab back in

---
+++ ui/tests/unit/__tests__/ExportFab.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ExportFab from "../../../src/components/ExportFab";
+
+test("Renders correctly", () => {
+  const tree = shallow(<ExportFab />);
+  expect(tree).toMatchSnapshot();
+});
