7bf3db63c26e69dacc023887e4a686c9fe2111ee
Alan Hwang <alanhwang@verily.com>
2018-07-24 14:26:53 -0700

    Export to Saturn tooltip

--- ui/src/components/ExportFab.js
+++ ui/src/components/ExportFab.js
@@ -4,13 +4,16 @@ import "./ExportFab.css";
 
 import CloudUpload from "@material-ui/icons/CloudUpload";
 import Button from "@material-ui/core/Button";
+import Tooltip from "@material-ui/core/Tooltip";
 import React from "react";
 
 function ExportFab(props) {
   return (
-    <Button variant="fab" color="secondary" className="exportFab">
-      <CloudUpload />
-    </Button>
+    <Tooltip title="Export to Saturn">
+      <Button variant="fab" color="secondary" className="exportFab">
+        <CloudUpload />
+      </Button>
+    </Tooltip>
   );
 }
 
