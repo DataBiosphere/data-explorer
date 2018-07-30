48350312d839a84c4e33f42006da85d64298e9a5
Alan Hwang <alanhwang@google.com>
2018-07-30 14:29:05 -0700

    Convert table names to request json and set up tests (#114)
    * Convert table names to request json and set up tests
    
    * Review fixes
    
    * Review fixes
    
    * Review fix
    
    * Review fix
    
    * Review fix

--- api/data_explorer/controllers/export_url_controller.py
+++ api/data_explorer/controllers/export_url_controller.py
@@ -1,5 +1,6 @@
 import connexion
 import six
+import json
 
 from flask import current_app
 
@@ -7,4 +8,17 @@ from data_explorer.models.export_url_response import ExportUrlResponse  # noqa: 
 
 
 def export_url_post():  # noqa: E501
-    return ExportUrlResponse(url='EXPORT_URL')
+    requests = []
+    for table_name in current_app.config['TABLE_NAMES']:
+        # Treat table names as BigQuery tables if they conform to project_id.dataset.table_id
+        if len(table_name.split(".")) == 3:
+            requests.append(
+                json.dumps({
+                    "name": table_name.split(".")[2],
+                    "entityType": "BigQuery table",
+                    "attributes": {
+                        "table_name": table_name
+                    }
+                }))
+    # TODO: Create a signed URL using the output of this request
+    return ExportUrlResponse(url="".join(requests))
---
+++ api/data_explorer/test/test_export_url_controller.py
@@ -0,0 +1,31 @@
+import json
+
+from data_explorer.test.base_test_case import BaseTestCase
+
+
+class TestExportUrlController(BaseTestCase):
+    """ExportUrlController integration test stubs"""
+
+    def create_app(self):
+        app = super(TestExportUrlController, self).create_app()
+        app.config.update({
+            'TABLE_NAMES': ['project_id.dataset_id.table_name']
+        })
+        return app
+
+    def test_export_url_post(self):
+        response = self.client.post('/exportUrl')
+        self.assert200(response)
+        expected_json = {
+            "name": 'table_name',
+            "entityType": "BigQuery table",
+            "attributes": {
+                "table_name": 'project_id.dataset_id.table_name'
+            }
+        }
+        self.assertEquals(expected_json, json.loads(response.json['url']))
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
