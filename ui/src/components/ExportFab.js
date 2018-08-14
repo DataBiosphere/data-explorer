80c3fce0cfdb5f66f3adf3ef7780c2758a383148
Malathi Raghavan <malathi13@users.noreply.github.com>
2018-08-14 15:19:05 -0700

    Added a dialog to provide cohort name when doing Export.

--- ui/src/components/ExportFab.js
+++ ui/src/components/ExportFab.js
@@ -5,16 +5,30 @@ import "./ExportFab.css";
 import CloudUpload from "@material-ui/icons/CloudUpload";
 import Button from "@material-ui/core/Button";
 import Tooltip from "@material-ui/core/Tooltip";
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import React from "react";
 
 class ExportFab extends React.Component {
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
+    this.state = {open: false};
+    this.handleClick = this.handleClick.bind(this);     
+    this.handleCancel = this.handleCancel.bind(this);    
+    this.handleSave = this.handleSave.bind(this);    
   }
+
+  state = {
+    open: false,
+  };
 
   render() {
     return (
+      <div>
       <Tooltip title="Export to Saturn">
         <Button
           variant="fab"
@@ -25,10 +39,42 @@ class ExportFab extends React.Component {
           <CloudUpload />
         </Button>
       </Tooltip>
+       <div>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Cohort Name</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Enter the cohort name
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Cohort Name"
+              type="text"
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleSave} color="primary">
+              Export
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+      </div>
     );
   }
 
   handleClick() {
+    this.setState(state => ({ open: true }));
+  }
+
+  handleSave() {
     let exportUrlCallback = function(error, data) {
       if (error) {
         console.error(error);
@@ -39,6 +85,7 @@ class ExportFab extends React.Component {
         );
       }
     }.bind(this);
+    this.setState(state => ({ open: false }));
     this.props.exportUrlApi.exportUrlPost(exportUrlCallback);
   }
 }
