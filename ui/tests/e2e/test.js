fb981e9b25e1f2b6ca4823ea148129a301cce13d
Bryan Crampton <bryancrampton@google.com>
2018-08-21 09:38:51 -0700

    Fix e2e tests

--- ui/tests/e2e/test.js
+++ ui/tests/e2e/test.js
@@ -10,31 +10,31 @@ describe("End-to-end", () => {
   });
 
   test("Header", async () => {
-    await expect(page).toMatch("Test data");
-    await assertHeaderTotalCount("1338");
+    await expect(page).toMatch("1000 Genomes");
+    await assertHeaderTotalCount("3500");
   });
 
-  test("Age facet", async () => {
-    await assertFacet("Age", "1338", "10-19", "137");
+  test("Gender facet", async () => {
+    await assertFacet("Gender", "3500", "female", "1760");
   });
 
   test("Faceted search", async () => {
-    // Click first Age facet value
-    let facetValueRow = await getFacetValueRow("Age", "10-19");
+    // Click first Super Population facet value.
+    let facetValueRow = await getFacetValueRow("Super Population", "African");
     await facetValueRow.click("input");
     // Wait for data to be returned from backend.
     // See #63 for why we can't wait for div.grayText.
     await page.waitForXPath(
-      "//div[contains(@class, 'totalCountText') and text() = '137']"
+      "//div[contains(@class, 'totalCountText') and text() = '1018']"
     );
 
-    // Assert page updated correctly
-    await assertHeaderTotalCount("137");
-    await assertFacet("Age", "137", "10-19", "137");
-    await assertFacet("Gender", "137", "male", "71");
+    // Assert page updated correctly.
+    await assertHeaderTotalCount("1018");
+    await assertFacet("Gender", "1018", "male", "518");
+    await assertFacet("Total Exome Sequence", "707", "0B-10B", "435");
 
-    // Make sure second Age facet value is gray
-    facetValueRow = await getFacetValueRow("Age", "20-29");
+    // Make sure second Super Population facet value is gray.
+    facetValueRow = await getFacetValueRow("Super Population", "European");
     const grayDiv = await facetValueRow.$("div.grayText");
     expect(grayDiv).toBeTruthy();
   });
@@ -85,11 +85,17 @@ describe("End-to-end", () => {
     firstValueName,
     firstValueCount
   ) {
-    let facetCard = await getFacetCard(facetName);
+    const facetCard = await getFacetCard(facetName);
 
-    expect(await facetCard.$eval("span.totalFacetValueCount", node => node.innerText)).toBe(totalCount);
-    expect(await facetCard.$eval("div.facetValueName", node => node.innerText)).toBe(firstValueName);
-    expect(await facetCard.$eval("div.facetValueCount", node => node.innerText)).toBe(firstValueCount);
+    expect(
+      await facetCard.$eval("span.totalFacetValueCount", node => node.innerText)
+    ).toBe(totalCount);
+    expect(
+      await facetCard.$eval("div.facetValueName", node => node.innerText)
+    ).toBe(firstValueName);
+    expect(
+      await facetCard.$eval("div.facetValueCount", node => node.innerText)
+    ).toBe(firstValueCount);
   }
 
   /**
@@ -116,10 +122,11 @@ describe("End-to-end", () => {
    * Returns Promise of JSHandle of facetCard div.
    */
   async function getFacetCard(facetName) {
-    let facetCard = (await page.evaluateHandle(facetName => {
-      let divs = document.querySelectorAll("div.facetCard");
-      for (let div of divs) {
-        if (div.innerText.match(facetName)) return div;
+    const facetCard = (await page.evaluateHandle(innerFacetName => {
+      const divs = document.querySelectorAll("div.facetCard");
+      for (const div of divs) {
+        const name = div.querySelector("span").innerText;
+        if (name.match(innerFacetName)) return div;
       }
       return null;
     }, facetName)).asElement();
