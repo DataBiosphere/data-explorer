dbf996ade6fecf336e119a27d6e4aeae682b4f34
melissachang <10929390+melissachang@users.noreply.github.com>
2018-08-22 10:04:08 -0700

    Add instructions for creating export url bucket (#124)

---
+++ deploy/create-export-url-bucket.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Creates GCS bucket for Export to Saturn feature.
+#
+# See export_url_controller.py for more information.
+#
+# jq, gsutil must be installed before running this script.
+
+if (( $# != 1 ))
+then
+  echo "Usage: deploy/create-export-url-bucket.sh <dataset>"
+  echo "  where <dataset> is the name of a directory in dataset_config/"
+  echo "Run this script from project root"
+  exit 1
+fi
+
+dataset=$1
+project_id=$(jq --raw-output '.project_id' dataset_config/${dataset}/deploy.json)
+bucket=gs://${project_id}-export
+
+if gsutil ls ${bucket} > /dev/null; then
+  echo "${bucket} already exists"
+  exit 0
+fi
+
+echo "Creating ${bucket}"
+gsutil mb -p ${project_id} ${bucket}
+
+echo "Setting bucket TTL to 1 day"
+gsutil lifecycle set deploy/export-url-bucket-lifecycle.json ${bucket}
+
+echo "Setting up CORS"
+gsutil cors set deploy/export-url-bucket-cors.json ${bucket}
