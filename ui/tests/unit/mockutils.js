1030d594689507843c463a42154d44f8bdf7b158
Malathi Raghavan <malathi13@users.noreply.github.com>
2018-09-12 11:26:04 -0700

    Updated mocks

--- ui/tests/unit/mockutils.js
+++ ui/tests/unit/mockutils.js
@@ -1,5 +1,6 @@
 import Facet from "../../src/api/src/model/Facet";
 import FacetValue from "../../src/api/src/model/FacetValue";
+import Field from "../../src/api/src/model/Field";
 
 // Utilities for setting up test instances
 
@@ -29,4 +30,20 @@ function getFacetValue(i) {
   value.name = "FacetValue " + i;
   value.count = i * 10;
   return value;
+}
+
+export function getFieldsList(numFields) {
+  let fields = [];
+  for (let i = 1; i <= numFields; i++) {
+    fields.push(getField("Field " + i));
+  }
+  return fields;
+}
+
+export function getField(fieldName) {
+  let field = new Field();
+  field.name = fieldName;
+  field.description = "description_" + fieldName;
+  field.elasticsearch_name = "es_name_" + fieldName;
+  return field;
 }
