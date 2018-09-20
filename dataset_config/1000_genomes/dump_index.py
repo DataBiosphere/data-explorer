ab18c3c6a8a7c092e96feeb0d7a4d3b5213fdc82
Malathi Raghavan <malathir@malathir-macbookpro2.roam.corp.google.com>
2018-09-20 10:43:49 -0700

    WIP

--- dataset_config/1000_genomes/dump_index.py
+++ dataset_config/1000_genomes/dump_index.py
@@ -6,8 +6,10 @@ from elasticsearch.helpers import scan
 
 ES_URL = 'http://localhost:9200'
 OUTPUT_INDEX_FILE = 'dataset_config/1000_genomes/index.json'
+OUTPU_FIELDS_INDEX_FILE = 'dataset_config/1000_genomes/fields.json'
 OUTPUT_MAPPINGS_FILE = 'dataset_config/1000_genomes/mappings.json'
 INDEX = '1000_genomes'
+FIELDS_INDEX = '1000_genomes_fields'
 DOC_TYPE = 'type'
 
 client = Elasticsearch([ES_URL])
@@ -18,6 +20,13 @@ for row in scan(client, query={}, index=INDEX, doc_type=DOC_TYPE):
     index_file.write('\n')
 
 index_file.close()
+
+fields_index_file = open(OUTPUT_FIELDS_INDEX_FILE, 'w')
+for row in scan(client, query={}, index=FIELDS_INDEX, doc_type=DOC_TYPE):
+    json.dump(row, fields_index_file)
+    fields_index_file.write('\n')
+
+fields_index_file.close()
 
 mappings_file = open(OUTPUT_MAPPINGS_FILE, 'w')
 mappings = requests.get('%s/%s/_mapping/type' % (ES_URL, INDEX)).json()
