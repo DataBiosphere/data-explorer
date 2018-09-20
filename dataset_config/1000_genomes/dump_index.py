7793f1e6705f55d9beff4b9edc173bc9b495fbed
Malathi Raghavan <malathi13@users.noreply.github.com>
2018-09-20 12:09:21 -0700

    Increased results to 100. Added the fields index to the dump and load scripts. Excluded the index json files from prettier

--- dataset_config/1000_genomes/dump_index.py
+++ dataset_config/1000_genomes/dump_index.py
@@ -24,7 +24,9 @@ for row in scan(client, query={}, index=INDEX, doc_type=DOC_TYPE):
 index_file.close()
 
 fields_index_file = open(OUTPUT_FIELDS_INDEX_FILE, 'w')
+print("Opened the fields index")
 for row in scan(client, query={}, index=FIELDS_INDEX, doc_type=DOC_TYPE):
+    print(row)
     json.dump(row, fields_index_file)
     fields_index_file.write('\n')
 
