7d1b9ba47f35982430d1a1f54e159b3fc5ebd622
Melissa Chang <melissachang@users.noreply.github.com>
2018-09-26 15:44:48 -0700

    Add extraFacets parameter to facets endpoint

--- api/data_explorer/controllers/dataset_controller.py
+++ api/data_explorer/controllers/dataset_controller.py
@@ -14,8 +14,7 @@ def dataset_get():  # noqa: E501
     :rtype: DatasetResponse
     """
     enable_field_search = False
-    if 'ENABLE_FIELD_SEARCH' in current_app.config and current_app.config[
-            'ENABLE_FIELD_SEARCH']:
+    if 'ENABLE_FIELD_SEARCH' in current_app.config and current_app.config['ENABLE_FIELD_SEARCH']:
         enable_field_search = True
     return DatasetResponse(
         name=current_app.config['DATASET_NAME'],
