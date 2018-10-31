4b1b949ab58ead4cd90d044ce6d957b9f2e6d9d2
Malathi Raghavan <malathir@malathir-macbookpro2.roam.corp.google.com>
2018-10-31 11:55:24 -0700

    Don't render a chip when a value is selected

--- ui/src/components/FieldSearch.js
+++ ui/src/components/FieldSearch.js
@@ -1,14 +1,5 @@
 import React from "react";
-import Select, { components } from "react-select";
-
-// MultiValue is used to set a custom chip label.
-// We want to show different text in chip vs drop-down, since description can be long.
-// See https://stackoverflow.com/questions/52482985/react-select-show-different-text-label-for-drop-down-and-control
-const MultiValue = props => (
-  <components.MultiValue {...props}>
-    {props.data.chipLabel}
-  </components.MultiValue>
-);
+import Select from "react-select";
 
 class FieldSearch extends React.Component {
   constructor(props) {
@@ -21,8 +12,8 @@ class FieldSearch extends React.Component {
         isMulti="true"
         onChange={this.props.handleChange}
         options={this.props.fields}
-        components={{ MultiValue }}
         value={this.props.extraFacetsOptions}
+        controlShouldRenderValue={false}
       />
     );
   }
