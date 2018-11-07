6b46e0eda3109c1d9e8dd433c924780b86d224b0
Daniel Moran <danxmoran@gmail.com>
2018-11-07 12:50:37 -0500

    Fix default values in API layer. (#204)
    * Change default value for sample_file_columns to a dictionary.
    * Read sample ID column from config on export.
    * Make sure EXTRA_FACET_INFO is initialized.

--- api/data_explorer/test/test_export_url_controller.py
+++ api/data_explorer/test/test_export_url_controller.py
@@ -58,6 +58,8 @@ class TestExportUrlController(BaseTestCase):
             'EXTRA_FACET_INFO': {},
             'ELASTICSEARCH_URL':
             'fakeurl:9200',
+            'SAMPLE_ID_COLUMN':
+            'sample_id',
         })
         return app
 
