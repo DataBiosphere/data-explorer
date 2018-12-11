2650c7a26ba190412d51694da248cf1ce9400e96
Malathi Raghavan <malathi13@users.noreply.github.com>
2018-12-11 12:48:07 -0800

    Formatted

--- api/data_explorer/controllers/dataset_controller.py
+++ api/data_explorer/controllers/dataset_controller.py
@@ -13,16 +13,18 @@ def dataset_get():  # noqa: E501
 
     :rtype: DatasetResponse
     """
-    
+
     enable_search = False
-    if 'ENABLE_SEARCH' in current_app.config and current_app.config['ENABLE_SEARCH']:
+    if 'ENABLE_SEARCH' in current_app.config and current_app.config[
+            'ENABLE_SEARCH']:
         enable_search = True
 
-    dataset_response = DatasetResponse(name=current_app.config['DATASET_NAME'], enable_search=enable_search)
+    dataset_response = DatasetResponse(
+        name=current_app.config['DATASET_NAME'], enable_search=enable_search)
 
     if 'SEARCH_PLACEHOLDER_TEXT' in current_app.config and current_app.config[
             'SEARCH_PLACEHOLDER_TEXT']:
         dataset_response.search_placeholder_text = current_app.config[
             'SEARCH_PLACEHOLDER_TEXT']
-    
+
     return dataset_response
