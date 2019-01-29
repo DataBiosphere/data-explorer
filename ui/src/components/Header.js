b95d10183c823e87e35887b634de962cdd8b7b8f
Melissa Chang <melissachang@users.noreply.github.com>
2019-01-29 14:47:42 -0800

    participant count left

--- ui/src/components/Header.js
+++ ui/src/components/Header.js
@@ -1,24 +1,41 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = {
+  appBar: {
+    backgroundColor: "#5aa6da"
+  },
+  datasetName: {
+    marginRight: 100
+  },
+  totalCount: {
+    fontSize: 17,
+    // Make bottom of dataset name line up with bottom of total count
+    paddingTop: "3px"
+  }
+};
 
 function Header(props) {
-  const datasetName = props.datasetName;
-  const totalCount = props.totalCount;
-
+  const { classes } = props;
   return (
-    <AppBar position="static" style={{ backgroundColor: "#5aa6da" }}>
+    <AppBar position="static" className={classes.appBar}>
       <Toolbar>
-        <Typography className="datasetName" variant="headline" color="inherit" style={{ flexGrow: 1 }}>
-          {datasetName}
+        <Typography
+          className={classes.datasetName}
+          variant="headline"
+          color="inherit"
+        >
+          {props.datasetName}
         </Typography>
-        <Typography className="totalCountText" color="inherit">
-          {totalCount} Participants
+        <Typography className={classes.totalCount} color="inherit">
+          {props.totalCount} Participants
         </Typography>
       </Toolbar>
     </AppBar>
   );
 }
 
-export default Header;
+export default withStyles(styles)(Header);
