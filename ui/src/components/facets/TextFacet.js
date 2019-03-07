a04d0ca13beeb52ca59562313088e58913d6d3f2
Melissa Chang <melissachang@users.noreply.github.com>
2019-03-07 11:22:24 -0800

    fix TextFacet colors

--- ui/src/components/facets/TextFacet.js
+++ ui/src/components/facets/TextFacet.js
@@ -8,10 +8,12 @@ import ListItemText from "@material-ui/core/ListItemText";
 import * as Style from "libs/style";
 import colors from "libs/colors";
 import FacetHeader from "components/facets/FacetHeader";
+import { CheckboxIcon } from "libs/icons";
 
 const styles = {
   textFacet: {
     ...Style.elements.card,
+    color: colors.gray[0],
     margin: "0 25px 28px 0",
     padding: 0,
     // If there is a long word (eg facet name or facet value), break in the
@@ -38,24 +40,19 @@ const styles = {
   },
   facetValueNameAndCount: {
     display: "grid",
-    gridTemplateColumns: "auto 50px",
-    paddingRight: 0
-  },
-  facetValueName: {
-    color: colors.gray[0],
-    fontSize: 14,
-    fontWeight: 500
-  },
-  facetValueCount: {
-    color: colors.gray[0],
     fontSize: 14,
     fontWeight: 500,
+    gridTemplateColumns: "auto 50px",
+    padding: "0 0 0 14px",
+    width: "100%"
+  },
+  facetValueName: {
+    // Used by integration test
+  },
+  facetValueCount: {
     textAlign: "right"
   },
-  listItemText: {
-    padding: "0 0 0 14px"
-  },
-  grayText: {
+  lightGrayText: {
     color: colors.gray[4]
   }
 };
@@ -86,21 +83,18 @@ class TextFacet extends Component {
             this.props.selectedValues != null &&
             this.props.selectedValues.includes(value.name)
           }
+          icon={CheckboxIcon}
         />
-        <ListItemText
-          classes={{
-            primary: this.isValueDimmed(value)
-              ? classes.facetValueNameAndCount + " " + classes.grayText
-              : classes.facetValueNameAndCount,
-            root: classes.listItemText
-          }}
-          primary={
-            <>
-              <div className={classes.facetValueName}>{value.name}</div>
-              <div className={classes.facetValueCount}>{value.count}</div>
-            </>
+        <div
+          className={
+            this.isValueDimmed(value)
+              ? classes.facetValueNameAndCount + " " + classes.lightGrayText
+              : classes.facetValueNameAndCount
           }
-        />
+        >
+          <div className={classes.facetValueName}>{value.name}</div>
+          <div className={classes.facetValueCount}>{value.count}</div>
+        </div>
       </ListItem>
     ));
 
