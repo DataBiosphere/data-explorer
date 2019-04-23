74c42c94c4a92fd6c442b06456207a07d7a1ae66
Melissa Chang <melissachang@users.noreply.github.com>
2019-04-23 11:46:37 -0700

    revert

--- ui/src/libs/common.js
+++ ui/src/libs/common.js
@@ -12,11 +12,7 @@ import colors from "libs/colors";
 const baseStyles = {
   buttonBase: {
     borderRadius: 5,
-    // For some reason different paddings are needed for Mac Chrome vs
-    // Linux Chrome, to make chip text vertically centered in button.
-    padding: navigator.platform.includes("Mac")
-      ? "0 15px 0 15px"
-      : "6px 15px 4px 15px",
+    padding: "6px 15px 4px 15px",
     textTransform: "uppercase",
     whiteSpace: "nowrap"
   }
