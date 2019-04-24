f8eba7d71756d22b20f459364bf562412507e923
Melissa Chang <melissachang@users.noreply.github.com>
2019-04-24 16:59:55 -0700

    hover bug

--- ui/jest-puppeteer.config.js
+++ ui/jest-puppeteer.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   launch: {
     // Uncomment to see browser.
-    // headless: false,
+    //     headless: false,
     // Uncomment to slow down execution. Unit is ms.
-    // slowMo: 200,
+    //     slowMo: 200,
     // Needed for debugger, see
     // https://github.com/GoogleChrome/puppeteer#debugging-tips
-    // devtools: true,
+    //     devtools: true,
   }
 };
