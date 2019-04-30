f95f1e6064ae2e1f0c08590a37bf439b92745ec5
Melissa Chang <melissachang@users.noreply.github.com>
2019-04-30 16:09:33 -0700

    Stop using mouseEnter/mouseLeave

--- ui/src/components/SaveButton.js
+++ ui/src/components/SaveButton.js
@@ -1,5 +1,4 @@
 import React from "react";
-import classNames from "classnames";
 import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
 import DialogContent from "@material-ui/core/DialogContent";
