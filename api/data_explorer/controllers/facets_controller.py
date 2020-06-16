d973ee1cd4ab68bbe36ab8fbdbe67b5b45b4106f
Willy Nojopranoto <nojopranoto@gmail.com>
2020-06-16 14:31:53 -0700

    Nit on comment

--- api/data_explorer/controllers/facets_controller.py
+++ api/data_explorer/controllers/facets_controller.py
@@ -186,7 +186,7 @@ def _get_facet_values(es_field_name, facet_info, es_response_facets):
             # elasticsearch-dsl returns boolean field keys as 0/1. Use the
             # field's 'type' to convert back to boolean, if necessary.
             # Also cast the bool to a str so that it renders as True/False
-            # instead of true/false (uppercase).
+            # instead of true/false.
             if facet_info['type'] == 'boolean':
                 interval_name = str(bool(value_name))
             else:
